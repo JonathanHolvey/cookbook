64bbf71a349daa6b73d4240454b05461b14a3cd5
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-11-14 14:11:02 +1000

    Move functions from recipe.php to common.php

--- recipe.php
+++ recipe.php
@@ -1,79 +1,11 @@
+<!DOCTYPE html>
 <?php
 	require_once("script/common.php");
 
 	$title = $_GET["title"];
 	$recipe = json_decode(file_get_contents("recipes/" . $title . ".json"), true);
 
-	// convert common fractions to html entities
-	function formatNumber($number) {
-		switch ($number) {
-			case 0.5: return "&frac12;";
-			case 0.25: return "&frac14;";
-			case 0.75: return "&frac34;";
-			case 0.33: return "&#8531;";
-			case 0.66: return "&#8532;";
-		}
-		return $number;
-	}
-
-	// converts strings such as tomato(es) and leaf/leaves to tomato and leaf or tomatoes and leaves
-	function formatString($string, $many) {
-		if ($many)
-			return preg_replace("/(\w+)\((\w*s)\)|(\w+)\/(\w+s)/", "$1$2$4", $string);
-		else
-			return preg_replace("/(\w+)\((\w*s)\)|(\w+)\/(\w+s)/", "$1$3", $string);
-	}
-
-	// finds ingredient details based on an id
-	function ingredientFromId($id) {
-		global $recipe;
-		// extract name from recipe ingredient list
-		$recipeXpath = $recipe->xpath("ingredients/item[@id=\"" . floor($id) . "\"]");
-		$name = reset($recipeXpath);
-
-		if (strpos($id, ".") !== false) {
-			// extract quantity, unit and prep from dish ingredient list
-			$dishXpath = $recipe->xpath("dish/ingredient[@id=\"" . $id . "\"]");
-
-			$details = 	array();
-			$details["name"] = (string)$name;
-			$details["quantity"] = isset(reset($dishXpath)->quantity)? reset($dishXpath)->quantity: null;
-			$details["unit"] = isset(reset($dishXpath)->unit)? reset($dishXpath)->unit: null;
-			$details["prep"] = isset(reset($dishXpath)->prep)? reset($dishXpath)->prep: null;
-			// returns array of all details if full id (10.2) is used
-			return $details;
-		}
-		else // returns just the ingredient name if short id (10) is used
-			return $name;
-	}
-
-	// converts numbers to text representation
-	function intToString($number) {
-		$strings = array("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
-		if ((int)$number >= 0 and (int)$number <= 10)
-			return $strings[(int)$number];
-		else
-			return $number;
-	}
-
-	// converts times of less than an hour to just minutes, eg 10 min
-	function convertTime($time) {
-		if (strpos($time, ":") !== false) {
-			preg_match("/([0-9]+):([0-9]{2})/", $time, $matches);
-			$hours = (int)$matches[1];
-			$minutes = (int)$matches[2];
-			if ($hours <= 0)
-				return $minutes . "&nbsp;min";
-		}
-		return $time;
-	}
-
-	// function for custom sorting of check list array
-	function checkListCompare($a, $b) {
-		return strcasecmp($a["name"], $b["name"]);
-	}
 ?>
-<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title><?= $recipe["title"] ?></title>
--- script/common.php
+++ script/common.php
@@ -10,4 +10,68 @@
 	function recipeCompare($a, $b) {
 		return strcmp($a->t, $b->t);
 	}
+
+	// convert common fractions to html entities
+	function formatNumber($number) {
+		switch ($number) {
+			case 0.5: return "&frac12;";
+			case 0.25: return "&frac14;";
+			case 0.75: return "&frac34;";
+			case 0.33: return "&#8531;";
+			case 0.66: return "&#8532;";
+		}
+		return $number;
+	}
+
+	// converts strings such as tomato(es) and leaf/leaves to tomato and leaf or tomatoes and leaves
+	function formatString($string, $many) {
+		if ($many)
+			return preg_replace("/(\w+)\((\w*s)\)|(\w+)\/(\w+s)/", "$1$2$4", $string);
+		else
+			return preg_replace("/(\w+)\((\w*s)\)|(\w+)\/(\w+s)/", "$1$3", $string);
+	}
+
+	// finds ingredient details based on an id
+	function parseIngredient($string) {
+		preg_match("/\[#(\d+)\|(?:([\d.]+) ?([^|]+)?\|)?(?:([^|]+)\|)?([\w\s]+)\]/", $string, $matches);
+
+		// extract quantity, unit and prep from dish ingredient list
+		$details = [];
+		$details["id"] = $matches[1];
+		$details["text"] = $matches[5];
+		$details["quantity"] = $matches[2];
+		$details["unit"] = $matches[3];
+		$details["prep"] = $matches[4];
+		return $details;
+	}
+
+	function getIngredientName($recipe, $id) {
+		return $recipe["ingredients"][0];
+	}
+
+	// converts numbers to text representation
+	function intToString($number) {
+		$strings = array("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
+		if ((int)$number >= 0 and (int)$number <= 10)
+			return $strings[(int)$number];
+		else
+			return $number;
+	}
+
+	// converts times of less than an hour to just minutes, eg 10 min
+	function convertTime($time) {
+		if (strpos($time, ":") !== false) {
+			preg_match("/([0-9]+):([0-9]{2})/", $time, $matches);
+			$hours = (int)$matches[1];
+			$minutes = (int)$matches[2];
+			if ($hours <= 0)
+				return $minutes . "&nbsp;min";
+		}
+		return $time;
+	}
+
+	// function for custom sorting of check list array
+	function checkListCompare($a, $b) {
+		return strcasecmp($a["name"], $b["name"]);
+	}
 ?>
