53b39837591f7b5b84a5ca50f16030c3d5d7fca9
Jonathan Holvey <jonathan.holvey@outlook.com>
2015-11-15 16:33:43 +1000

    Prevent latest page from trying to show more recipes than exist

--- pages/latest.php
+++ pages/latest.php
@@ -1,7 +1,7 @@
 <div class="page" id="latest">
 	<h2>Recent additions to the cookbook</h2>
 	<?php
-		for ($i = 0; $i < 5; $i ++): ?>
+		for ($i = 0; $i < min(5, count($recipeIndex)); $i ++): ?>
 			<div class="recent-box" style="background-image:url('<?= $recipeIndex[$i]["p"] ?>')">
 				<a href="recipes/<?= str_replace(".json", "", $recipeIndex[$i]["f"]) ?>" class="title"><?= $recipeIndex[$i]["t"] ?></a>
 			</div>
