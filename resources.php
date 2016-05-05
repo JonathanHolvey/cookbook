1f81155adc01112d890ac3f59ca37309258f7333
Jonathan Holvey <jonathan.holvey@outlook.com>
2016-05-05 19:06:54 +1000

    Correct sizes attribute of icon link

--- resources.php
+++ resources.php
@@ -6,5 +6,5 @@
 <script type="text/javascript" src="script/jquery.mobile.touch.js"></script>
 <script type="text/javascript" src="script/common.js"></script>
 <link rel="shortcut icon" href="images/icon.ico"/>
-<link rel="icon" type="image/png" href="images/icon.png" sizes="162x192"/>
+<link rel="icon" type="image/png" href="images/icon.png" sizes="192x192"/>
 <meta name="theme-color" content="#45ADA8"/>
