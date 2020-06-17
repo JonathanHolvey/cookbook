3c3865d43b6a5174b6eda299584acbfb4a6cac1e
Jonathan Holvey <jonathan.holvey@outlook.com>
2020-06-17 23:13:43 +1000

    Update base tag to include proxied protocol

--- script/common.php
+++ script/common.php
@@ -2,7 +2,8 @@
 	// find the base url of the site
 	function findBase() {
 		$domain = "cookbook.rocketchilli.com";
-		$url = "http" . (isset($_SERVER["HTTPS"])? "s": "") . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+		$https = $_SERVER["HTTPS"] || $_SERVER['HTTP_X_FORWARDED_PROTO'] === "https";
+		$url = "http" . ($https ? "s" : "") . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 		$base = substr($url, 0, strpos($url, $domain) + strlen($domain));
 		return $base . "/";
 	}
